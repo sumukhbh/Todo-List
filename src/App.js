8abdb84e1a4da5c79027074fe3f70a79a4a64dc4
Sumukh Bharadwaj <sumukh745@gmail.com>
2020-03-27 15:08:48 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App () {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p> placeholder text </p>
+      <input type="checkbox" />
+      <p> placeholder text </p>
+      <input type="checkbox" />
+      <p> placeholder text </p>
+      <input type="checkbox" />
+      <p> placeholder text </p>
+    </div>
+  )
+}
+
+export default App
