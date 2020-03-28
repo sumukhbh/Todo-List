9325ba5ebf7e76f5cf721fee1194b38ef3498c19
Sumukh Bharadwaj <sumukh745@gmail.com>
2020-03-28 15:56:37 -0700

    Added Todoitems

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react"
+import TodoItem from "./TodoItem.js"
+
+const todosData = [
+  {
+      id: 1,
+      text: "Take out the trash",
+      completed: true
+  },
+  {
+      id: 2,
+      text: "Grocery shopping",
+      completed: false
+  },
+  {
+      id: 3,
+      text: "Clean gecko tank",
+      completed: false
+  },
+  {
+      id: 4,
+      text: "Mow lawn",
+      completed: true
+  }
+  
+]
 
 function App () {
+  const todoarr = todosData.map(item => <TodoItem key= {item.id} item={item}/>)
   return (
-    <div>
-      <input type="checkbox" />
-      <p> placeholder text </p>
-      <input type="checkbox" />
-      <p> placeholder text </p>
-      <input type="checkbox" />
-      <p> placeholder text </p>
-      <input type="checkbox" />
-      <p> placeholder text </p>
+    <div className="todo-list">
+      {todoarr}
     </div>
   )
 }
